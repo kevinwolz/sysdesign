25ef7337b9db4562ea69b13a28d0c70340130c52
Kevin Wolz <wolzkevin@gmail.com>
2018-11-17 13:47:13 +0000

    Initial commit nelder functions.

---
+++ R/nelder.R
@@ -0,0 +1,166 @@
+#' Create a Nelder Fan experimental design
+#' @description Creates a Nelder Fan experimental design.
+#' @details The Nelder Fan or Nelder Wheel experimental design (Nelder 1962) is an experimental design that systematically
+#' varies plant desnity within a single plot. The terminology and calculations used here follow Parrott, Brinks, and Lhotka (2012).
+#' @return An object of class "sysd" and class "nelder". This is a list of 3 elements:
+#' \itemize{
+#'  \item{"plants"}{ - A data frame (tibble) containing one row for each for each plant in the design.}
+#'  \item{"plot"}{ - A data frame (tibble) containing plot charateristics.}
+#'  \item{"optim"}{ - If \code{even = TRUE}, a data frame (tibble) comparing plot characteristics between the scenario
+#'  where theta is even throughout the design and the scenario where theta is uneven.
+#'  If \code{even = FALSE}, then \code{FALSE}.}
+#' }
+#' @param DN Plant density within the last experimental arc (plants ha-1) (i.e. lower extreme of experimental plant density range)
+#' @param D1 Plant density within the first experimental arc (plants ha-1) (i.e. upper extreme of experimental plant density range)
+#' @param N Number of experimental arcs (i.e. number of densities to be tested within D1 to DN)
+#' @param tau The "rectangularity" proportion. "Rectangularity" is the proportional relationship between the arc length between
+#'  spokes and the radial length between arcs, where the numerator represents the arc length and the denominator represents
+#'  radial distance. This proportion has been referred to as "rectangularity" in the historical literature and it remains constant
+#'  throughout the design. Given that the inner and outer borders of the growing space shape surrounding a plant in a Nelder
+#'  design are arcs, and that the shape is not truly rectangular or trapezoidal in nature, the term "rectangularity" can be
+#'  confusing.
+#' @param even Logical indicated whether or not the design should be adjusted so that the angle between spokes goes evenly
+#' into \code{max.angle} (i.e. so that there are no spokes that must be removed from the experiment as border spokes).
+#' @param max.angle The maximum rotation (in degrees) of the design. If 360, then a full circle design will be created.
+#' @author Kevin J Wolz, \email{kevin@@savannainstitute.org}
+#' @references
+#' \itemize{
+#'  \item Nelder JA (1962) New kinds of systematic designs for spacing experiments.
+#'  Biometrics 18:283-307.
+#'  \url{http://www.jstor.org/stable/2527473}
+#'  \item Parrott DL, Brinks JS, Lhotka JM (2011) Designing Nelder wheel plots for tree density experiments.
+#'  New Forests 43:245–254.
+#'  \url{http://link.springer.com/10.1007/s11056-011-9278-4}
+#' }
+#' @export
+#' @importFrom dplyr %>%
+#' @family definition functions
+#' @examples
+#' sysd <- nelder(DN         = 1000,
+#'                D1         = 3000,
+#'                N          = 5,
+#'                tau        = 1,
+#'                even       = TRUE,
+#'                max.angle  = 360)
+nelder <- function(DN, D1, N, tau = 1, even = FALSE, max.angle = 360) {
+
+  if(!(is.numeric(D1) & (D1 %% 1 == 0) & length(D1) == 1))         stop("D1 must an integer and of length 1",           call. = FALSE)
+  if(!(is.numeric(DN) & (DN %% 1 == 0) & length(DN) == 1))         stop("DN must an integer and of length 1",           call. = FALSE)
+  if(!(is.numeric(N)  & (N  %% 1 == 0) & length(N)  == 1 & N > 2)) stop("N must be an integer greater than 2",          call. = FALSE)
+  if(D1 <= DN)                                                     stop("D1 must be greater than DN",                   call. = FALSE)
+  if(!(is.numeric(tau) & length(tau) == 1))                        stop("tau argument must be numeric and of length 1", call. = FALSE)
+  if(!is.logical(even))                                            stop("even argument must be a logical",              call. = FALSE)
+  if(max.angle > 360 | max.angle <= 0)                             stop("max.angle argument must be between 0 and 360", call. = FALSE)
+
+  max.angle.rad <- max.angle * pi / 180  # radians
+
+  ## ORIGINAL VALUES WITHOUT EVEN SPOKE OPTIMIZATION
+  tau.orig      <- tau
+  alpha.orig    <- exp((log(D1) - log(DN)) / (2 * N - 2))
+  theta.orig    <- tau.orig * (alpha.orig ^ 0.5 - alpha.orig ^ -0.5) # radians
+  n.spokes.orig <- ceiling(max.angle.rad / theta.orig)
+
+  orig.dat <- nelder_calc(alpha         = alpha.orig,
+                          theta         = theta.orig,
+                          tau           = tau.orig,
+                          D1            = D1,
+                          N             = N,
+                          max.angle     = max.angle,
+                          n.spokes      = n.spokes.orig,
+                          spoke.borders = TRUE)
+
+  ## NEW VALUES WITH EVEN SPOKE OPTIMIZATION
+  n.spokes <- round(max.angle.rad / theta.orig)
+  theta    <- max.angle.rad / n.spokes
+
+  get_alpha <- function(alpha) abs((alpha ^ 0.5 - alpha ^ -0.5) - theta / tau.orig)
+  alpha <- optimize(get_alpha, c(0, alpha.orig * 10000))$minimum
+  tau   <- theta / (alpha ^ 0.5 - alpha ^ -0.5)
+
+  even.dat <- nelder_calc(alpha         = alpha,
+                          theta         = theta,
+                          tau           = tau,
+                          D1            = D1,
+                          N             = N,
+                          max.angle     = max.angle,
+                          n.spokes      = n.spokes,
+                          spoke.borders = max.angle != 360)
+
+  if(even) {
+    even.spoke.dat <- dplyr::bind_rows(orig.dat$plot, even.dat$plot) %>%
+      dplyr::mutate(even = c(FALSE, TRUE)) %>%
+      dplyr::select(even, dplyr::everything())
+
+    out <- c(even.dat, list(optim = even.spoke.dat))
+
+  } else {
+    out <- c(orig.dat, list(optim = FALSE))
+  }
+
+  class(out) <- c(class(out), "nelder", "sysd")
+  return(out)
+}
+
+#' Calculate Nelder Fan design
+#' @description Calculates Nelder Fan design
+#' Used within \code{\link{nelder}}.
+#' @return A list containing the plant and plot data.
+#' @param alpha Rate of change along the spokes.
+#' @param theta The angle between the spokes.
+#' @param tau The "rectangularity" proportion
+#' @param D1 Plant density within the first experimental arc (plants ha-1) (i.e. upper extreme of experimental plant density range)
+#' @param N Number of experimental arcs (i.e. number of densities to be tested within D1 to DN)
+#' @param max.angle The maximum rotation (in degrees) of the design. If 360, then a full circle design will be created.
+#' @param n.spokes The number of spokes.
+#' @param spoke.borders Logical indicating whether or not there are spokes required to be border spokes.
+#' @importFrom dplyr %>%
+#' @keywords internal
+nelder_calc <- function(alpha, theta, tau, D1, N, max.angle, n.spokes, spoke.borders) {
+
+  Co <- (2 - (alpha + alpha ^ -1)) / (2 * (alpha - alpha ^ -1)) * 100
+  r0 <- sqrt(20000 / (D1 * theta * (alpha ^ 3 - alpha))) # m
+
+  arc.dat <- dplyr::tibble(arc = c(0:(N + 1))) %>%
+    dplyr::mutate(r       = r0 * alpha ^ arc) %>%                         # m
+    dplyr::mutate(space   = theta * r ^ 2 * (alpha - alpha ^ -1) / 2) %>% # m2
+    dplyr::mutate(density = 10000 / space)                                # plants ha-1
+
+  arc.dat$space[c(1, N + 2)]   <- NA
+  arc.dat$density[c(1, N + 2)] <- NA
+
+  plot.dat <- dplyr::tibble(area           = pi * tail(arc.dat$r, 1) ^ 2 / 10000 * max.angle / 360, # ha
+                            arcs           = N + 2,
+                            exp.arcs       = N,
+                            spokes         = n.spokes,
+                            exp.spokes     = ifelse(spoke.borders, n.spokes - 2, n.spokes),
+                            r0             = r0,
+                            rmax           = max(arc.dat$r),
+                            alpha          = alpha,
+                            angle          = theta * 180 / pi,
+                            rectangularity = tau,
+                            non.centrality = Co,
+                            min.density    = min(arc.dat$density, na.rm = TRUE),
+                            max.density    = D1,
+                            plants         = (N + 2) * n.spokes)
+
+  if(max.angle < 360) plot.dat$spokes <- plot.dat$spokes + 1
+
+  plant.dat <- dplyr::tibble(spoke  = rep(1:plot.dat$spokes, each  = plot.dat$arcs),
+                             arc    = rep(arc.dat$arc,       times = plot.dat$spokes),
+                             theta  = rep(plot.dat$angle * 0:(plot.dat$spokes - 1), each = plot.dat$arcs)) %>%
+    dplyr::left_join(arc.dat, by = "arc")
+
+  ## DESIGNATE EXPERIMENTAL VS BORDER ROWS
+  if(spoke.borders) {
+    plant.dat <- dplyr::mutate(plant.dat, exp = (theta %in% range(theta)) | (arc %in% range(arc)))
+  } else {
+    plant.dat <- dplyr::mutate(plant.dat, exp = arc %in% range(arc))
+  }
+  plant.dat <- dplyr::mutate(plant.dat, exp = factor(exp, levels = c(FALSE, TRUE), labels = c("Experimental", "Border")))
+
+  plot.dat <- plot.dat %>%
+    dplyr::mutate(exp.plants = sum(plant.dat$exp == "Experimental")) %>%
+    dplyr::mutate(perc.exp   = round(exp.plants / plants * 100))
+
+  return(list(plants = plant.dat, plot = plot.dat))
+}
---
+++ R/plot.R
@@ -0,0 +1,76 @@
+#' Plot a Nelder Fan experimental design
+#' @description Plots a Nelder Fan experimental design.
+#' @return A ggplot2 object.
+#' @param data An object of class 'nelder' created via \code{\link{nelder}}.
+#' @param size A numeric value indicating point size.
+#' @param fill.colors A character vector of length 2 indicating the fill colors of experimental and border plants, respectively.
+#' @param legend Logical indicating whether or not to include a legend for fill colors.
+#' @param ex.area Logical indicating whether or not and example growing area of one plant should be shown.
+#' @param caption Logical indicating whether or not to include a caption with generic data about the design.
+#' @author Kevin J Wolz, \email{kevin@@savannainstitute.org}
+#' @export
+#' @import ggplot2
+#' @family plot functions
+#' @examples
+#' data <- nelder(D1         = 3000,
+#'                DN         = 1000,
+#'                N          = 5,
+#'                tau        = 1,
+#'                even       = TRUE,
+#'                max.angle  = 360)
+#' plot_nelder(data)
+plot_nelder <- function(data,
+                        size        = 3,
+                        fill.colors = c("black", "white"),
+                        legend      = FALSE,
+                        ex.area     = FALSE,
+                        caption     = FALSE) {
+
+  if(!("nelder" %in% class(data)))                            stop("data must be of class 'nelder'",                     call. = FALSE)
+  if(!(is.numeric(size) & length(size) == 1))                 stop("size must be numeric and of length 1",               call. = FALSE)
+  if(!(is.character(fill.colors) & length(fill.colors) == 2)) stop("fill.colors must be a character vector of length 2", call. = FALSE)
+  if(!is.logical(ex.area))                                    stop("ex.area must be a logical",                          call. = FALSE)
+  if(!is.logical(caption))                                    stop("caption must be a logical",                          call. = FALSE)
+
+  plot.caption <- NULL
+  if(caption) {
+    plot.caption <- paste0("# Spokes: ",        data$plot$spokes, " (", data$plot$exp.spokes, ")",
+                           "\n# Arcs: ",        data$plot$arcs,   " (", data$plot$exp.arcs, ")",
+                           "\n# Angle: ",       round(data$plot$angle, 2),  " degrees",
+                           "\n# Plants: ",      data$plot$plants, " (", data$plot$exp.plants, ")",
+                           "\nDensity range: ", round(data$plot$min.density), " - ", data$plot$max.density, " plants/ha",
+                           "\nr0-rmax: ",       round(data$plot$r0, 2),       " - ", round(data$plot$rmax, 2), " m",
+                           "\nPlot area: ",     round(data$plot$area, 2), " ha")
+  }
+
+  area.geom <- geom_blank()
+  if(ex.area) {
+    r <- unique(data$plants$r)
+    i <- length(r) - 1
+    radius.max <- (r[i] + r[i + 1]) / 2
+    radius.min <- (r[i] + r[i - 1]) / 2
+
+    area.shade <- dplyr::tibble(ymin = rep(data$plot$angle - data$plot$angle / 2, 2),
+                                ymax = rep(data$plot$angle + data$plot$angle / 2, 2),
+                                x    = c(radius.min, radius.max))
+
+    area.geom <- geom_ribbon(aes(ymin = ymin, ymax = ymax, x = x),
+                             data = area.shade,
+                             fill = "grey50")
+  }
+
+  plot.obj <- ggplot(data$plants) +
+    area.geom +
+    geom_point(aes(x = r, y = theta, fill = exp), shape = 21, size = size) +
+    scale_fill_manual(values = fill.colors) +
+    lims(x = c(0, NA),
+         y = c(0, 360)) +
+    coord_polar(theta = "y", start = -pi / 2, direction = -1) +
+    theme_void() +
+    labs(fill = NULL, caption = plot.caption) +
+    theme(plot.caption    = element_text(size = 20, hjust = 0),
+          legend.position = ifelse(legend, "bottom", "none"),
+          legend.text     = element_text(size = 18))
+
+  return(plot.obj)
+}
